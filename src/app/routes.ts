5ac627b6c05e43724bc83d01c1805e66e118bfb1
ConfusedHorse <21200630+ConfusedHorse@users.noreply.github.com>
2025-04-17 14:14:48 +0200

    temporary truncation

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const ROUTES: Routes = [
   { path: '', redirectTo: 'lizard', pathMatch: 'full' },
-  { path: 'mandelbrot', loadChildren: () => import('../mandelbrot/routes').then(m => m.ROUTES) },
-  { path: 'something', loadChildren: () => import('../something/routes').then(m => m.ROUTES) },
+  // { path: 'mandelbrot', loadChildren: () => import('../mandelbrot/routes').then(m => m.ROUTES) },
+  // { path: 'something', loadChildren: () => import('../something/routes').then(m => m.ROUTES) },
   { path: 'lizard', loadChildren: () => import('../lizard/routes').then(m => m.ROUTES) },
 ];
--- src/lizard/shader/fragment.ts
+++ src/lizard/shader/fragment.ts
@@ -5,9 +5,9 @@ export const fragmentShader = `
   varying vec3 v_worldPosition;
 
   void main() {
-    float red = u_opacity * v_worldPosition.y / u_canvasHeight;
+    float green = u_opacity * v_worldPosition.y / u_canvasHeight;
     float blue = u_opacity * v_worldPosition.x / u_canvasWidth;
 
-    gl_FragColor = vec4(red, 0.0, blue, 1);
+    gl_FragColor = vec4(0.0, green, blue, 1);
   }
 `;
